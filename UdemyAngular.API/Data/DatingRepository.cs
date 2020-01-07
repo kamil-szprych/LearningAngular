60d0607308c2327cc08364d75d226af3ebfbe93d
Kamil Szprych <kamil.szprych@gmail.com>
2020-01-07 15:58:48 +0100

    Implemented paging, flitering and sorting for member list (section 14)

--- UdemyAngular.API/Data/DatingRepository.cs
+++ UdemyAngular.API/Data/DatingRepository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UdemyAngular.API.Helpers;
 using UdemyAngular.API.Models;
 
 namespace UdemyAngular.API.Data
@@ -29,7 +30,7 @@ namespace UdemyAngular.API.Data
 
         public async Task<Photo> GetMainPhotoForUser(int userId)
         {
-            return _context.Photos.Where(u => u.UserId == userId).FirstOrDefault(p => p.IsMain);
+            return await _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
         }
 
         public async Task<Photo> GetPhoto(int id)
@@ -44,10 +45,34 @@ namespace UdemyAngular.API.Data
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = await _context.Users.Include(u => u.Photos).ToListAsync();
-            return users;
+            var users = _context.Users.Include(u => u.Photos).OrderByDescending(u=> u.LastActive).AsQueryable();
+            users = users.Where(u => u.Id != userParams.UserId);
+            users = users.Where(u => u.Gender == userParams.Gender);
+            
+            if(userParams.MinAge != 18 || userParams.MaxAge != 99)
+            {
+                var minDate = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+                var maxDate = DateTime.Today.AddYears(-userParams.MinAge);
+
+                users = users.Where(u => u.DateOfBirth >= minDate && u.DateOfBirth <= maxDate);
+            }
+
+            if(!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch (userParams.OrderBy)
+                {
+                    case "created":
+                        users = users.OrderByDescending(u => u.Created);
+                        break;
+                    default:
+                        users = users.OrderByDescending(u => u.LastActive);
+                        break;
+                }
+            }
+
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
--- UdemyAngular.API/Data/IDatingRepository.cs
+++ UdemyAngular.API/Data/IDatingRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using UdemyAngular.API.Helpers;
 using UdemyAngular.API.Models;
 
 namespace UdemyAngular.API.Data
@@ -9,7 +10,7 @@ namespace UdemyAngular.API.Data
         void Add<T>(T entity) where T : class;
         void Delete<T>(T entity) where T : class;
         Task<bool> SaveAll();
-        Task<IEnumerable<User>> GetUsers();
+        Task<PagedList<User>> GetUsers(UserParams userParams);
         Task<User> GetUser(int id);
         Task<Photo> GetPhoto(int id);
         Task<Photo> GetMainPhotoForUser(int userId);
